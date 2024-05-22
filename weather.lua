5fc610a1093765bf60bfaf8ebf640a0b5f6aff6f
gdanko <Gary_Danko@intuit.com>
2024-05-22 14:20:36 -0700

    Remove debug code; add reference URLs

--- weather.lua
+++ weather.lua
@@ -1,16 +1,20 @@
+-- https://openweathermap.org/api/geocoding-api
+-- https://api.openweathermap.org/geo/1.0/direct?q=San%20Diego,CA,US&limit=4&appid=xxxxxx
+-- https://api.openweathermap.org/geo/1.0/direct?q=San+Diego,US&limit=5&appid=xxxxx
+-- https://api.openweathermap.org/geo/1.0/zip?zip=92103&appid=xxxxx
+-- https://api.openweathermap.org/geo/1.0/reverse?lat=32.74&long=-117.24&appid=xxxxx
+
+local util = require "util"
 local wezterm = require "wezterm"
 
 local weather = {}
 
 function weather.write_weather_file(weather_file)
     url = string.format("https://api.openweathermap.org/geo/1.0/direct?q=%s&limit=1&appid=%s", location, appid)
-    wezterm.log_info(url)
     success, stdout, stderr = wezterm.run_child_process({"curl", url})
     if success then
-        wezterm.log_info(111)
         json_data = util.json_parse_string(stdout)
         if json_data ~= nil then
-            wezterm.log_info(json_data)
             location_data = json_data[1]
             if location_data["lon"] ~= nil and location_data["lat"] ~= nil then
                 url = string.format("https://api.openweathermap.org/data/2.5/weather?lon=%s&lat=%s&units=imperial&appid=%s", location_data["lon"], location_data["lat"], appid)
@@ -33,7 +37,8 @@ function weather.write_weather_file(weather_file)
     return nil
 end
 
-function weather.get_icon(icon_id)
+function weather.get_icon(icon_id, condition_id)
+    -- https://openweathermap.org/weather-conditions
     if icon_id == "01d" then
         return "☀️"
     elseif icon_id == "01n" then
