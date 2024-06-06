bc646650eb67c1089a2e70d0921c89d818ae745c
gdanko <Gary_Danko@intuit.com>
2024-06-05 17:56:18 -0700

    Add the function to get wifi status to status-bar.lua as putting it in its own module would cause an error when trying to write the file

--- status-bar.lua
+++ status-bar.lua
@@ -5,7 +5,6 @@ local github = require "github"
 local util = require "util"
 local weather = require "weather"
 local wezterm = require "wezterm"
-local wifi_status = require "wifi-status"
 
 local wsstats_json_file = "/tmp/wsstats.json"
 
@@ -291,7 +290,48 @@ function status_bar.update_status_bar(cwd)
                 if system_status_config["toggles"]["show_wifi_status"] then
                     action, wifi_status_data = util.determine_action(config["status_bar"]["system_status"]["wifi_status"])
                     if action == "update" then
-                        wifi_status.update_wifi_status()
+                        local output = {
+                            timestamp = nil,
+                            interfaces = {}
+                        }
+                        data_file = config["status_bar"]["system_status"]["wifi_status"]["data_file"]
+                        if config["os_name"] == "linux" then
+                            network_interface_list = config["status_bar"]["system_status"]["network_interface_list"]
+                            if network_interface_list ~= nil then
+                                for _, ifname in ipairs(network_interface_list) do
+                                    success, stdout, stderr = wezterm.run_child_process({"iwconfig", ifname})
+                                    if success then
+                                        signal_level = stdout:match("Signal level=--(%d+) dBm")
+                                        if signal_level ~= nil then
+                                            output["interfaces"][ifname] = signal_level
+                                        end
+                                    end
+                                end
+                            end
+                        elseif config["os_name"] == "darwin" then
+                            success, stdout, stderr = wezterm.run_child_process({"/usr/sbin/system_profiler", "SPAirPortDataType", "-json", "-detailLevel", "basic"})
+                            if success then
+                                data = util.json_parse_string(stdout)
+                                if data ~= nil then
+                                    interfaces  = data["SPAirPortDataType"][1]["spairport_airport_interfaces"]
+                                    for _, interface in ipairs(interfaces) do
+                                        ifname = interface["_name"]
+                                        if util.has_value(config["status_bar"]["system_status"]["network_interface_list"], ifname) then
+                                            spairport_signal_noise = interface["spairport_current_network_information"]["spairport_signal_noise"]
+                                            signal_level = spairport_signal_noise:match("-(%d+) dBm")
+                                            if signal_level ~= nil then
+                                                output["interfaces"][ifname] = signal_level
+                                            end
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                        output["timestamp"] = util.get_timestamp()
+                        wezterm.log_info(output)
+                        local file = io.open(data_file, "w")
+                        file:write(wezterm.json_encode(output))
+                        file:close()
                     elseif action == "display" then
                         for iface, signal_level in pairs(wifi_status_data["interfaces"]) do
                             signal_level = tonumber(signal_level)
